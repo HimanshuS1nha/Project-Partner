149b982fa2704424a3b86b92c7d401cc297533cb
Himanshu Sinha <ashusinha1904@gmail.com>
2024-10-05 01:33:40 +0530

    Sending email functionality added

---
+++ src/lib/send-email.ts
@@ -0,0 +1,25 @@
+import { createTransport } from "nodemailer";
+
+export const sendEmail = async (email: string, otp: number) => {
+  try {
+    const transporter = createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: process.env.SECURE_CONNECTION ? true : false,
+      requireTLS: true,
+      auth: {
+        user: process.env.SMTP_EMAIL,
+        pass: process.env.SMTP_PASS,
+      },
+    });
+    await transporter.sendMail({
+      from: process.env.SMTP_EMAIL,
+      to: email,
+      subject: "Verify Your Email",
+      html: `<p>Your otp is: ${otp}</p>`,
+    });
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
