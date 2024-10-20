8fad543ebb0cc5f30c9c99aa8508faf6925808b5
Himanshu Sinha <ashusinha1904@gmail.com>
2024-10-21 01:00:22 +0530

    Responsiveness added to Dashboard Navbar

--- src/components/dashboard/Navbar.tsx
+++ src/components/dashboard/Navbar.tsx
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import { IoFolderOpen } from "react-icons/io5";
+import { IoFolderOpen, IoMenuOutline } from "react-icons/io5";
 import { IoMdSettings } from "react-icons/io";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import axios, { AxiosError } from "axios";
@@ -17,6 +17,14 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+  SheetClose,
+} from "@/components/ui/sheet";
 import Loader from "../ui/loader";
 import { useUser } from "@/hooks/useUser";
 
@@ -70,8 +78,8 @@ const Navbar = () => {
           <p className="text-xl font-bold">
             Project<span className="text-indigo-600 font-bold">Buddy</span>
           </p>
-          <p className="text-gray-700">/</p>
-          <div className="flex items-center gap-x-2">
+          <p className="text-gray-700 hidden md:block">/</p>
+          <div className="hidden md:flex items-center gap-x-2">
             {isLoading ? (
               <Loader size="sm" />
             ) : (
@@ -82,7 +90,7 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div className="flex gap-x-7">
+        <div className="hidden md:flex gap-x-7">
           <Link
             href={"/dashboard/projects"}
             className={`flex gap-x-2 items-center relative ${
@@ -113,30 +121,94 @@ const Navbar = () => {
         </div>
       </div>
 
-      {isLoading ? (
-        <Loader size="sm" />
-      ) : (
-        <DropdownMenu>
-          <DropdownMenuTrigger className="focus:outline-none">
-            <div className="bg-indigo-600 w-8 h-8 rounded-full flex justify-center items-center">
-              <p className="text-white">{user?.name?.[0]}</p>
+      <div className="flex items-center gap-x-5">
+        {isLoading ? (
+          <Loader size="sm" />
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger className="focus:outline-none">
+              <div className="bg-indigo-600 w-8 h-8 rounded-full flex justify-center items-center">
+                <p className="text-white">{user?.name?.[0]}</p>
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="cursor-pointer" asChild>
+                <Link href={"/change-password"}>Change Password</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="focus:bg-rose-600 focus:text-white cursor-pointer"
+                onClick={() => handleLogout()}
+              >
+                Logout
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+
+        <Sheet>
+          <SheetTrigger>
+            <IoMenuOutline
+              size={30}
+              color="black"
+              className="block lg:hidden"
+            />
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>
+                <p className="text-xl font-bold">
+                  Project
+                  <span className="text-indigo-600 font-bold">Buddy</span>
+                </p>
+              </SheetTitle>
+            </SheetHeader>
+
+            <div className="flex flex-col gap-y-4 mt-6">
+              <SheetClose asChild>
+                <Link
+                  href={"/dashboard/projects"}
+                  className={`flex gap-x-2 items-center ${
+                    pathname.includes("/dashboard/projects")
+                      ? "text-indigo-600"
+                      : "hover:bg-indigo-100"
+                  } p-1.5 cursor-pointer delay-100 transition-all rounded-lg`}
+                >
+                  <IoFolderOpen
+                    color={
+                      pathname.includes("/dashboard/projects")
+                        ? "blue"
+                        : "black"
+                    }
+                    size={23}
+                  />
+                  <p className="font-semibold">Projects</p>
+                </Link>
+              </SheetClose>
+
+              <SheetClose asChild>
+                <Link
+                  href={"/dashboard/settings"}
+                  className={`flex gap-x-2 items-center ${
+                    pathname === "/dashboard/settings"
+                      ? "text-indigo-600"
+                      : "hover:bg-indigo-100"
+                  } p-1.5 cursor-pointer delay-100 transition-all rounded-lg`}
+                >
+                  <IoMdSettings
+                    color={
+                      pathname === "/dashboard/settings" ? "blue" : "black"
+                    }
+                    size={23}
+                  />
+                  <p className="font-semibold">Account Settings</p>
+                </Link>
+              </SheetClose>
             </div>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="cursor-pointer" asChild>
-              <Link href={"/change-password"}>Change Password</Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem
-              className="focus:bg-rose-600 focus:text-white cursor-pointer"
-              onClick={() => handleLogout()}
-            >
-              Logout
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      )}
+          </SheetContent>
+        </Sheet>
+      </div>
     </nav>
   );
 };
