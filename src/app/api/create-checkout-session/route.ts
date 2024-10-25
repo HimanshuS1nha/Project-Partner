0c0cb804bb8aff8b83d695dfb7338d8e25f1c47e
Himanshu Sinha <ashusinha1904@gmail.com>
2024-10-25 22:39:59 +0530

    New api route /api/create-checkout-session added

---
+++ src/app/api/create-checkout-session/route.ts
@@ -0,0 +1,50 @@
+import { cookies } from "next/headers";
+import { jwtVerify } from "jose";
+import { NextResponse } from "next/server";
+
+import prisma from "@/lib/db";
+import { stripe } from "@/lib/stripe";
+
+export const POST = async () => {
+  try {
+    const token = cookies().get("token")?.value;
+    if (!token) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const { payload } = await jwtVerify(
+      token,
+      new TextEncoder().encode(process.env.JWT_SECRET)
+    );
+
+    const user = await prisma.users.findUnique({
+      where: {
+        email: payload.email as string,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: "price_1QDpf8SJN0Kv6QsT4f3movEw",
+        },
+      ],
+      mode: "subscription",
+      success_url: `${process.env.APP_URL}/projects/dashboard`,
+      cancel_url: `${process.env.APP_URL}/projects/dashboard`,
+      client_reference_id: user.id,
+    });
+
+    return NextResponse.json({ url: session.url }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Some error occured. Please try again later!" },
+      { status: 500 }
+    );
+  }
+};
